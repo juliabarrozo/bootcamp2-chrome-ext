24ee2d90b2caf4c150d3c0a5453c97613778be59
Julia <juliabarrozorp@gmail.com>
2025-11-16 23:51:32 -0300

    feat: ajusta pwa

--- apps/web/public/service-worker.js
+++ apps/web/public/service-worker.js
@@ -1,9 +1,18 @@
-// apps/web/public/service-worker.js
+const CACHE_NAME = "meu-pwa-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon-192x192.png"];
 
-self.addEventListener('install', () => {
-  console.log('Service Worker instalado!');
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', () => {
-  // Aqui futuramente vamos adicionar cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
