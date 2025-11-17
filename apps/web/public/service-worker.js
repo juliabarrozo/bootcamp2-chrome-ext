ebc923dd25a1a20fe4fec15e6efbe75176f1931c
Julia <juliabarrozorp@gmail.com>
2025-11-16 22:15:17 -0300

    Deploy site no gh-pages

--- apps/web/public/service-worker.js
+++
@@ -1,9 +0,0 @@
-// apps/web/public/service-worker.js
-
-self.addEventListener('install', () => {
-  console.log('Service Worker instalado!');
-});
-
-self.addEventListener('fetch', () => {
-  // Aqui futuramente vamos adicionar cache
-});
